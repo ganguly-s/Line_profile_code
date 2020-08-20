51ad61f3f81dbf3953a7207cab87ddd2457b57b9
Shalini Ganguly <ganguly93@sekacz.physics.unlv.edu>
2020-08-20 14:35:23 -0700

    changed threading for better performance

--- thread_all.py
+++ thread_all.py
@@ -13,13 +13,21 @@ import time
 import get_inp as gi
 import line_profile_op as lptst
 
-num_threads = 24
+Model = ['Ax8', 'Bx8']
+Line = ['Mg_XII','Si_XIV_6','Si_XIV_5','Fe_XXV','Fe_XXVI','O_VII','Ne_VIII','S_IV','S_IV*','Si_III','C_IV','C_VI','O_VI','He_II','N_V','O_VIII_19','Si_IV','N_VII','O_VIII_15','O_VIII_16','Ne_X','S_XVI_4','Ar_XVIII','Mg_II','C_II']
+Line = ['Fe_XXV']
+num_threads = 30
 
 q = queue.Queue()
 
-Model = ['Ax8', 'Bx8']
-Line = ['Mg_XII','Si_XIV_6','Si_XIV_5','Fe_XXV','Fe_XXVI','O_VII','Ne_VIII','S_IV','S_IV*','Si_III','C_IV','C_VI','O_VI','He_II','N_V','O_VIII_19','Si_IV','N_VII','O_VIII_15','O_VIII_16','Ne_X','S_XVI_4','Ar_XVIII','Mg_II','C_II']
-Line = ['C_IV']
+if 0:
+	Model = 'Ax8'
+	match  = '*.out1.00840.*'
+	num_threads = len(Line)
+if 0:
+	Model = 'Bx8'
+	match = '*.out1.*'
+	num_threads = 30
 start = time.time()
 def worker():
 	while True:
@@ -34,10 +42,12 @@ for i in range(num_threads):
 
 for mod in Model:
 	lst = fnmatch.filter(os.listdir(gi.get_Model(mod)), '*.out1.*')
+	#lst = fnmatch.filter(os.listdir(gi.get_Model(mod)), match)
 	lst.sort()
 	for i,sn in enumerate(lst):
-		for l in Line:
-			q.put([mod, sn, l])
+		if i>1298 :
+			for l in Line:
+				q.put([mod, sn, l])
 q.join()
 print("Done executing thread")
 finish = time.time()
