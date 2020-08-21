7d851a26e31a0bb6cad0ae9d842f7137033c8fbd
Shalini Ganguly <ganguly93@sekacz.physics.unlv.edu>
2020-08-20 18:00:09 -0700

    ready to run thread for less work, long way to optimization

--- thread_all.py
+++ thread_all.py
@@ -15,7 +15,7 @@ import line_profile_op as lptst
 
 Model = ['Ax8', 'Bx8']
 Line = ['Mg_XII','Si_XIV_6','Si_XIV_5','Fe_XXV','Fe_XXVI','O_VII','Ne_VIII','S_IV','S_IV*','Si_III','C_IV','C_VI','O_VI','He_II','N_V','O_VIII_19','Si_IV','N_VII','O_VIII_15','O_VIII_16','Ne_X','S_XVI_4','Ar_XVIII','Mg_II','C_II']
-Line = ['Fe_XXV']
+Line = ['Si_XIV_5']
 num_threads = 30
 
 q = queue.Queue()
@@ -24,7 +24,7 @@ if 0:
 	Model = 'Ax8'
 	match  = '*.out1.00840.*'
 	num_threads = len(Line)
-if 0:
+if 1:
 	Model = 'Bx8'
 	match = '*.out1.*'
 	num_threads = 30
@@ -41,8 +41,8 @@ for i in range(num_threads):
 	t.start()
 
 for mod in Model:
-	lst = fnmatch.filter(os.listdir(gi.get_Model(mod)), '*.out1.*')
-	#lst = fnmatch.filter(os.listdir(gi.get_Model(mod)), match)
+	#lst = fnmatch.filter(os.listdir(gi.get_Model(mod)), '*.out1.*')
+	lst = fnmatch.filter(os.listdir(gi.get_Model(mod)), match)
 	lst.sort()
 	for i,sn in enumerate(lst):
 		if i>1298 :
